4ceafd90af0310f035255bc1da1ab6b62e0bef66
Vladimir Sokolov <vladimir.sokolov.od@gmail.com>
2016-11-17 15:39:56 +0200

    first commit

---
+++ src/main/java/springjms/Application.java
@@ -0,0 +1,28 @@
+package springjms;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {		
+	public static void main(String[] args) throws URISyntaxException, Exception{
+				
+		BrokerService broker = BrokerFactory.createBroker(new URI("broker:(tcp://localhost:61616)"));
+		broker.start();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		try {
+			JmsProducer jmsProducer = context.getBean(JmsProducer.class);
+			JmsReceiver jmsReceiver = context.getBean(JmsReceiver.class);
+						
+			jmsProducer.sendMessage("Fisrt JMS message");
+			System.out.println("Received message: " + jmsReceiver.receiveMessage());			
+		} finally {
+			broker.stop();
+			context.close();
+		}
+	}
+}
